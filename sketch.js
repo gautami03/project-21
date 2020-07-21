3c5a43235e623804a4e9ceaada535373ab7883c6
gautami03 <66514498+gautami03@users.noreply.github.com>
2020-07-22 00:05:19 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,44 @@
+
+
+function setup() {
+  createCanvas(1600,400);
+  bullet=createSprite(50,200,50,50);
+  thickness=random(22,83);
+  speed=random(223,321);
+  weight=random(30,52);
+  wall=createSprite(1200,200,thickness,height/2);
+  bullet.velocityX=speed;
+  wall.shapeColor=(80,80,80);
+}
+
+function draw() {
+  background(255,255,255);  
+  
+
+  if(wall.x-bullet.x < (bullet.width+wall.width)/2){
+    bullet.velocityX=0;
+    var damage=0.5*weight*speed*speed/(thickness*thickness*thickness)
+  }
+
+  if(damage>=10){
+    wall.shapeColor="red";
+  }
+  if(damage<10){
+    wall.shapeColor="green";
+  }
+ 
+
+  hasCollided();
+  drawSprites();
+  
+}
+
+function hasCollided(lbullet, lwall){
+bulletRightEdge=bullet.x + bullet.width
+wallLeftEdge=wall.x;
+if(bulletRightEdge>=wallLeftEdge){
+return true
+}
+
+return false
+}
